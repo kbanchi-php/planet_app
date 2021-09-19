905d68d5dad6c1d46bc214850b7909d36306b3c1
kbanchi1111 <kbanchi.1111@gmail.com>
2021-09-19 22:28:13 +0900

    課題修正

--- resources/views/planets/create.blade.php
+++ resources/views/planets/create.blade.php
@@ -27,19 +27,19 @@
         @csrf
         <div class="planet-field">
             <label for="name_ja">名前</label>
-            <input type="text" name="name_ja" value="{{ old('name_ja') }}">
+            <input type="text" name="name_ja" value="{{ old('name_ja') }}" required>
         </div>
         <div class="planet-field">
             <label for="name_en">名前(英語)</label>
-            <input type="text" name="name_en" value="{{ old('name_en') }}">
+            <input type="text" name="name_en" value="{{ old('name_en') }}" required>
         </div>
         <div class="planet-field">
             <label for="radius_km">半径</label>
-            <input type="number" name="radius_km" value="{{ old('radius_km') }}">
+            <input type="number" name="radius_km" value="{{ old('radius_km') }}" required>
         </div>
         <div class="planet-field">
             <label for="wight">重量</label>
-            <input type="number" name="weight" value="{{ old('weight') }}">
+            <input type="number" name="weight" value="{{ old('weight') }}" required>
         </div>
         <input type="submit" value="登録">
     </form>
--- resources/views/planets/edit.blade.php
+++ resources/views/planets/edit.blade.php
@@ -29,19 +29,19 @@
             @method('PATCH')
             <div class="planet-field">
                 <label for="name_ja">名前</label>
-                <input type="text" name="name_ja" value="{{ old('name_ja', $planet->name_ja) }}">
+                <input type="text" name="name_ja" value="{{ old('name_ja', $planet->name_ja) }}" required>
             </div>
             <div class="planet-field">
                 <label for="name_en">名前(英語)</label>
-                <input type="text" name="name_en" value="{{ old('name_en', $planet->name_en) }}">
+                <input type="text" name="name_en" value="{{ old('name_en', $planet->name_en) }}" required>
             </div>
             <div class="planet-field">
                 <label for="radius_km">半径</label>
-                <input type="number" name="radius_km" value="{{ old('radius_km', $planet->radius_km) }}">
+                <input type="number" name="radius_km" value="{{ old('radius_km', $planet->radius_km) }}" required>
             </div>
             <div class="planet-field">
                 <label for="wight">重量</label>
-                <input type="number" name="weight" value="{{ old('weight', $planet->weight) }}">
+                <input type="number" name="weight" value="{{ old('weight', $planet->weight) }}" required>
             </div>
             <input type="submit" value="更新">
         </form>
